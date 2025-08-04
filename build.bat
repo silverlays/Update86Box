4f96acc63ad7d500b5b471d89a300253d1954c63
Damien Lacroix <inforlac.lacroix@gmail.com>
2025-08-04 16:51:45 +0200

    removed build batch (internal reason)

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-python3 -m nuitka --onefile ^
- --windows-console-mode=disable ^
- --enable-plugin=pyside6 ^
- --windows-icon-from-ico=app.ico ^
- --output-filename=86BoxUpdater.exe ^
- --deployment ^
- --remove-output ^
- app.pyw
